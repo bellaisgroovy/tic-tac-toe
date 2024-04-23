67656769796556aafd162cf6eaeb75310be34763
Bella <2770706h@student.gla.ac.uk>
2024-04-23 17:28:44 +0100

    patch with decorator

--- test/tic_tac_toe/player/test_normal_human_player.py
+++ test/tic_tac_toe/player/test_normal_human_player.py
@@ -4,42 +4,42 @@ from src.tic_tac_toe.player.normal_human_player import NormalHumanPlayer
 
 
 class TestNormalHumanPlayer(unittest.TestCase):
-    def test_move_validInput_returnDict(self):
+    @patch('src.tic_tac_toe.player.normal_human_player.NormalHumanPlayer.input_wrap', return_value="7")
+    def test_move_validInput_returnDict(self, mock_input):
         player = NormalHumanPlayer()
         board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-        with patch('src.tic_tac_toe.player.normal_human_player.NormalHumanPlayer.input_wrap', return_value="7"):
-            move_dict = player.move(board)
+        move_dict = player.move(board)
 
         expected_dict = {'x': 0, 'y': 0}
         self.assertEqual(move_dict, expected_dict)
 
-    def test_move_wrongTypeInput_askAgain(self):
+    @patch('src.tic_tac_toe.player.normal_human_player.NormalHumanPlayer.input_wrap', side_effect=["pizza", "5"])
+    def test_move_wrongTypeInput_askAgain(self, mock_input):
         player = NormalHumanPlayer()
         board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-        with patch('src.tic_tac_toe.player.normal_human_player.NormalHumanPlayer.input_wrap', side_effect=["pizza", "5"]):
-            move_dict = player.move(board)
+        move_dict = player.move(board)
 
         expected_dict = {'x': 1, 'y': 1}
         self.assertEqual(move_dict, expected_dict)
 
-    def test_move_highInput_askAgain(self):
+    @patch('src.tic_tac_toe.player.normal_human_player.NormalHumanPlayer.input_wrap', side_effect=["10", "1"])
+    def test_move_highInput_askAgain(self, mock_input):
         player = NormalHumanPlayer()
         board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-        with patch('src.tic_tac_toe.player.normal_human_player.NormalHumanPlayer.input_wrap', side_effect=["10", "1"]):
-            move_dict = player.move(board)
+        move_dict = player.move(board)
 
         expected_dict = {'x': 0, 'y': 2}
         self.assertEqual(move_dict, expected_dict)
 
-    def test_move_lowInput_askAgain(self):
+    @patch('src.tic_tac_toe.player.normal_human_player.NormalHumanPlayer.input_wrap', side_effect=["0", "3"])
+    def test_move_lowInput_askAgain(self, mock_input):
         player = NormalHumanPlayer()
         board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-        with patch('src.tic_tac_toe.player.normal_human_player.NormalHumanPlayer.input_wrap', side_effect=["0", "3"]):
-            move_dict = player.move(board)
+        move_dict = player.move(board)
 
         expected_dict = {'x': 2, 'y': 2}
         self.assertEqual(move_dict, expected_dict)
