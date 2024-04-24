5225bdcabe95f8d4309b5c907669f68f759cb93d
Bella <2770706h@student.gla.ac.uk>
2024-04-24 17:49:24 +0100

    added comment

--- src/tic_tac_toe/board/normal_board.py
+++ src/tic_tac_toe/board/normal_board.py
@@ -3,4 +3,4 @@ from src.tic_tac_toe.board.board import Board
 
 class NormalBoard(Board):
     def get_board(self):
-        return [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        return [[0, 0, 0], [0, 0, 0], [0, 0, 0]]  # 0 = empty, no enum so no players can be dynamic
