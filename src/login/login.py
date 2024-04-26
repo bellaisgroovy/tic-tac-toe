7cd2d1b0f6c36e136b30abd1b70811cccb9d9558
bella at work <bella.hodgson@glasgow.ac.uk>
2024-04-26 16:45:34 +0100

    login

---
+++ src/login/login.py
@@ -0,0 +1,10 @@
+class Login:
+    def __init__(self, loader):
+        self.loader = loader
+
+    def login(self):
+        return
+
+    def get_obj_list(self, path):
+        return self.loader.get_obj_list(path)
+
